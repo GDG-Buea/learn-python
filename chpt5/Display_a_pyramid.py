1661c539ace88d95d14b08adbf69f6d1abd753b9
Mandy <shafack.likhene@gmail.com>
2018-05-21 06:07:11 +0100

    a better way of displaying a  pyramid

--- chpt5/Display_a_pyramid.py
+++ chpt5/Display_a_pyramid.py
@@ -1,20 +1,20 @@
 # This program prompts the user to enter an integer from 1 to 15 and displays a pyramid,
 
-pyramidSize = int(eval(input("Enter the number of lines you want your pyramid to have: ")))
+pyramidSize =int(eval(input("Enter the number of lines you want your pyramid to have: ")))
 
 for i in range(1, pyramidSize + 1):
 
     # spaces to the left and right
     for j in range(1, (pyramidSize + 1) - i):
-        print(" ", end=" ")
+        print("", end='\t')
 
     # printing numbers to the left
 
     for k in range(i, 1, -1):
-        print(format(k, "1d"), end=" ")
+        print(format(k, "1d"), end='\t')
     # printing numbers to the right
 
     for l in range(1, i + 1):
-        print(format(l, "1d"), end=" ")
+        print(format(l, "1d"), end='\t')
 
     print()
